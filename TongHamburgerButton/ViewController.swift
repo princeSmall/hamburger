e929f22fc6d2a013bdd5d52e6a2d870dfee1e8f1
Tongle <Tongle@sansi.com>
2017-06-09 17:06:48 +0800

    Initial Commit

---
+++ TongHamburgerButton/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TongHamburgerButton
+//
+//  Created by tongle on 2017/6/9.
+//  Copyright © 2017年 tong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
